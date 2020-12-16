3501a202c69f765ee4e177aa831e6de062b76839
ddark <ddark.kr@gmail.com>
2020-12-17 04:10:41 +0900

    feat: 최신 버전 대응 업데이트

--- check.py
+++ check.py
@@ -1,16 +1,19 @@
 import hcskr
 
 import discord
-from env import CHECK_NAME, CHECK_SCHOOL_NAME, CHECK_SCHOOL_AREA, CHECK_SCHOOL_LEVEL, CHECK_BIRTH
+from env import CHECK_NAME, CHECK_SCHOOL_NAME, CHECK_SCHOOL_AREA, CHECK_SCHOOL_LEVEL, CHECK_BIRTH, CHECK_PASSWORD
 
 
 def check():
-    return_data = hcskr.selfcheck(CHECK_NAME, CHECK_BIRTH, CHECK_SCHOOL_AREA, CHECK_SCHOOL_NAME, CHECK_SCHOOL_LEVEL)
-    if return_data['code'] == 'SUCCESS':
-        print("[!] 자가진단 성공")
-        print("message: " + return_data['message'])
-        discord.success(return_data['message'])
-    else:
-        print("[!] 자가진단 실패")
-        print("message: " + return_data['message'])
-        discord.failed(return_data['message'])
+    try:
+        return_data = hcskr.selfcheck(CHECK_NAME, CHECK_BIRTH, CHECK_SCHOOL_AREA, CHECK_SCHOOL_NAME, CHECK_SCHOOL_LEVEL, CHECK_PASSWORD)
+        if return_data['code'] == 'SUCCESS':
+            print("[!] 자가진단 성공")
+            print("message: " + return_data['message'])
+            discord.success(return_data['message'])
+        else:
+            print("[!] 자가진단 실패")
+            print("message: " + return_data['message'])
+            discord.failed(return_data['message'])
+    except:
+        discord.failed('알 수 없는 문제가 발생했습니다.')
